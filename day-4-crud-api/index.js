4810fa35ade0e8f5b23da591ef56e9a113fd4140
Ekram Asrar <ekramasrar94@gmail.com>
2025-07-02 02:39:03 -0700

    Day 4: Task manager CRUD API

--- day-4-crud-api/index.js
+++ day-4-crud-api/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
 const tasks = [];
-
-// POST(Add tasks)
+// POST Add a new Task
 
 app.post('/tasks', (req, res) => {
     const { title } = req.body;
     if (!title)
-        res.status(404).json({ error: 'This is title is required' });
+        res.status(404).json({ error: 'Title is required' });
     const newTask = {
         id: tasks.length + 1,
         title,
@@ -18,37 +18,35 @@ app.post('/tasks', (req, res) => {
     tasks.push(newTask);
     res.status(201).json(newTask);
 });
+// Get (Get all tasks)
 
-// Get(get all tasks)
-
-app.get('/tasks', (req, res) => {
-    res.json(tasks);
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
 });
 
-// PUT update a task
+// Put (Update tasks)
 
-app.put('/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id);
-    const { title, completed } = req.body;
+app.put("/tasks/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const { title, completed } = req.body;
 
-    const task = tasks.find(t => t.id === taskId)
-    if (!task)
-        return res.status(404).json({ error: 'Task not found' });
-    if (title !== undefined)
-        task.title = title;
-    if (completed !== undefined)
-        task.completed = completed;
-    res.json(task);
+  const task = tasks.find((t) => t.id === taskId);
+  if (!task) return res.status(404).json({ error: "Task not found" });
+
+  if (title !== undefined) task.title = title;
+  if (completed !== undefined) task.completed = completed;
+
+  res.json(task);
 });
 
-app.delete('/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id);
-    tasks = tasks.filter(t => t.id !== taskId);
-    res.json({ message: 'Task deleted successfully' });
-});
+app.delete("/tasks/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+  tasks = tasks.filter((t) => t.id !== taskId);
 
-app.listen(3000, (req, res) => {
-    console.log('Server is running...');
+  res.json({ message: "Task deleted successfully" });
 });
 
 
+app.listen(3000, () => {
+    console.log('server is running......');
+});
